8980bf25356cf584d325c36cf5581b04907e15f7
Pulkit Jaiswal <pulkitj@icloud.com>
2013-05-25 00:05:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gesture::Application.config.secret_token = 'c89172c5cd563214d79e605c1fdb2f34eaf5e87dd29136b45db1ce0c9ce0e2fb89f14cf92c17a3c8e171fc819e51f9318eeb489e07b6544d556f08bc87170e65'
